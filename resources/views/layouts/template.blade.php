89833f07f3fa07dd9bad6f8858343304807b39c3
DhyasR <119585072+DhyasR@users.noreply.github.com>
2024-05-14 19:43:02 +0700

    refactor: new update code

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,9 +10,34 @@
 
     <title>Document</title>
 </head>
+
 <body>
 
+    <header class="header">
+
+        <img src="../../resources/images/logo.png" alt="logo" id="logo">
+
+        <input type="checkbox" id="check">
+        <label for="check" class="icons">
+
+            <i class='bx bx-menu' id="open-menu"></i>
+            <i class='bx bx-x' id="close-menu"></i>
+
+        </label>
+
+        <nav class="navbar">
+
+            <a href="" style="--i:0">Home</a>
+            <a href="" style="--i:1">About</a>
+            <a href="" style="--i:2">Services</a>
+            <a href="" style="--i:3">Contact</a>
+
+        </nav>
+
+    </header>
+
     @yield('content')
-    
+
 </body>
+
 </html>
